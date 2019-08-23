0ec54e472c5ef525aeca5a9e91734b51cbb561f3
azymv <timazcimaz@gmail.com>
2019-08-23 12:27:18 +0300

    Update settings.py

--- keido/settings.py
+++ keido/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'jjqbc5i7lbv1+bgzr##*$@pt4v%2rulx5%w-&cw(sq)ok579yd'
+
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
